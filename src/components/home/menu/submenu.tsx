4a3a4f316936f114250b1b28de6c58e4dc5868bc
nedled <matheusmned@gmail.com>
2026-02-17 19:26:57 -0300

    ajustes menu

--- src/components/home/menu/submenu.tsx
+++ src/components/home/menu/submenu.tsx
@@ -4,7 +4,6 @@ import style from "./submenu.module.scss";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { MenuCms } from "@/utils/types/menu";
-//teste
 
 export default function SubMenu({ objects }: MenuCms) {
   const [toggleMenu, setToggleMenu] = useState(false);
