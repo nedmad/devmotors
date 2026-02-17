ead0f1320292dc33c1c0569f68db25723f627269
nedled <matheusmned@gmail.com>
2026-02-17 19:13:11 -0300

    segundo teste

--- src/components/home/subservices/subservices.tsx
+++ src/components/home/subservices/subservices.tsx
@@ -1,6 +1,3 @@
-import Container from "@/components/container/container";
-import style from "./subservices.module.scss";
-import Image from "next/image";
 import { HomeCms } from "@/utils/types/cms";
 export default function SubServices({ object }: HomeCms) {
   return <></>;
